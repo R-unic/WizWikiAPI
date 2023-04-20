173fb0f82a3a1f1ec8c098f9b08e747913b6bbdc
r-unic <roleyspersonal@gmail.com>
2023-04-19 18:48:20 -0700

    build: add typedoc configuration

--- app/src/App.ts
+++ app/src/App.ts
@@ -13,17 +13,6 @@ import path from "path";
 const app = express();
 const port = env.PORT || 3000; //
 const routes: CommonRoutesConfig[] = [];
-const loggerOptions: LoggerOptions = {
-  transports: [new transports.Console()],
-  format: format.combine(
-    format.json(),
-    format.prettyPrint(),
-    format.colorize({ all: true })
-  )
-};
-
-if (!env.DEBUG)
-  loggerOptions.meta = false;
 
 function addStyles(...styleNames: string[]): void {
   for (const style of styleNames)
@@ -38,7 +27,7 @@ function addStyles(...styleNames: string[]): void {
 
 app.use(express.json());
 app.use(cors());
-app.use(logger(loggerOptions));
+app.use("/docs", express.static(path.join(__dirname, "../docs")));
 
 try {
   const routesPath = path.join(__dirname, "..", "dist", "Routes");
@@ -53,11 +42,28 @@ try {
 
 try {
   const homepageHTML = readFileSync(__dirname + "/../index.html", { encoding: "utf8" });
-  const docsHTML = readFileSync(__dirname + "/../docs.html", { encoding: "utf8" });
+  const docsHTML = readFileSync(__dirname + "/../docs/index.html", { encoding: "utf8" });
+  const docsHighlightCSS = readFileSync(__dirname + "/../docs/assets/highlight.css", { encoding: "utf8" });
+  const docsCSS = readFileSync(__dirname + "/../docs/assets/style.css", { encoding: "utf8" });
+  const mainJS = readFileSync(__dirname + "/../docs/assets/main.js", { encoding: "utf8" });
+  const searchJS = readFileSync(__dirname + "/../docs/assets/search.js", { encoding: "utf8" });
   addStyles("main", "docs");
-  app.get("/docs", (_, res) =>res.status(ResponseCode.SUCCESS).send(docsHTML))
+
+  app.get("/docs", (_, res) => res.status(ResponseCode.SUCCESS)
+    .setHeader("Content-Type", "text/html")
+    .send(
+      docsHTML
+        .replace(/\<title\>wizwikiapi\<\/title\>/g, "<title>Wiz Wiki API Docs</title>")
+        .replace(/\<link rel\=\"stylesheet\" href=\"assets\/highlight\.css"\>/g, `<style>${docsHighlightCSS}</style>`)
+        .replace(/\<link rel\=\"stylesheet\" href=\"assets\/style\.css"\>/g, `<style>${docsCSS}</style>`)
+        .replace(/\<script src\=\"assets\/main\.js\"\>\<\/script\>/g, `<style>${mainJS}</style>`)
+        .replace(/\<script src\=\"assets\/search\.js\"\>\<\/script\>/g, `<style>${searchJS}</style>`)
+    ));
+
   app.get("/", (_, res) =>
-    res.status(ResponseCode.SUCCESS).send(homepageHTML)
+    res.status(ResponseCode.SUCCESS)
+      .setHeader("Content-Type", "text/html")
+      .send(homepageHTML)
   ).listen(port, () => {
     routes.forEach(route => Logger.Log(`Routes configured for ${route.Name}`));
     Logger.Log(`Server is listening @http://localhost:${port}`);
